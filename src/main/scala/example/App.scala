94210c909b7aa527e0b47723a59e93f97378fc51
kartavtcev <kartavtcev.sergei@gmail.com>
2019-07-15 04:26:36 +0300

    handleErrorWith error handling. Code refactoring.

--- src/main/scala/example/App.scala
+++ src/main/scala/example/App.scala
@@ -1,17 +1,17 @@
 package example
 
+import cats.data.NonEmptyList
 import cats.effect._
 import cats.syntax.flatMap._
 import example.config.ElevioConfig
 import example.infra.{HttpClient, Pools}
 import example.services.GatewayService
 import io.chrisdavenport.log4cats.Logger
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import io.circe.config.parser
 
 trait App extends {
 
-  def run[F[_]: ConcurrentEffect: ContextShift: Timer](implicit logger: Logger[F]) = {
+  def run[F[_]: ConcurrentEffect: ContextShift](implicit logger: Logger[F]) = {
 
     /**
       * EC should be different for different services. OK for the demo.
@@ -30,15 +30,13 @@ trait App extends {
 
         val service = GatewayService[F](HttpClient[F], ec, appcf)
         Resource.liftF(
-          //service.articleSearchByKeyword(NonEmptyList.of("article"))
-          service.articleList()
+          service.articleSearchByKeyword(NonEmptyList.of("article"))
+          //service.articleList()
             .flatMap(response => Logger[F].info(s"Response text: ${response}")) // TODO: remove
         )
     }
   }
 
-  def program[F[_]: ConcurrentEffect: ContextShift: Timer] = {
-    implicit val logger = Slf4jLogger.getLogger[F]
-    run[F]
-  }
+  def program[F[_]: ConcurrentEffect: ContextShift](implicit logger: Logger[F]) = run[F]
+
 }
--- src/main/scala/example/AppIO.scala
+++ src/main/scala/example/AppIO.scala
@@ -2,9 +2,15 @@ package example
 
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.syntax.functor._
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 
 object AppIO extends IOApp with App {
 
-  override def run(args: List[String]): IO[ExitCode] = program[IO].use(_ => IO.unit.as(ExitCode.Success))
-  //.handleErrorWith(_ => IO.unit)  // TODO: handle & log errors.
+  override def run(args: List[String]): IO[ExitCode] = {
+    implicit val logger = Slf4jLogger.getLogger[IO]
+    program[IO]
+      .use(_ => IO.unit)
+      .handleErrorWith(ex => logger.error(ex)(ex.getMessage))
+      .as(ExitCode.Success)
+  }
 }
